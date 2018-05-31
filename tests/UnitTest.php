135da259a95a51bd3bde5ea2d014474e380e057d
Tacxticx88 <tacxticx88@toroia.fr>
2018-05-31 14:31:43 +0200

    add unit tests

---
+++ tests/UnitTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+    public function testTest()
+    {
+        $this->assertEquals(
+            "works",
+            "works",
+            "This is OK"
+        );
+    }
+}
